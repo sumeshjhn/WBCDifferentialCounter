a3a55252ca34e3883f69e7d0f84fb7cfcec5eb09
XXX xxx <xxx@xxx.xx>
2015-05-14 00:47:51 -0400

    second commit.  Added most of UI.  Need to style portrait and build functionality

---
+++ app/src/main/java/com/bohn/boomesh/wbcdifferentialcounter/HomeActivityFragment.java
@@ -0,0 +1,18 @@
+package com.bohn.boomesh.wbcdifferentialcounter;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeActivityFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        // TODO add logic for portrait
+        return view;
+    }
+}
